204206b2e11a2e9da4da452f60daf944cd9cdc6f
Luis Parrado <luisprmat@gmail.com>
2024-04-06 00:40:08 -0500

    Align icon

--- src/components/AccordionItem.vue
+++ src/components/AccordionItem.vue
@@ -18,12 +18,10 @@ const toggleAccordion = () => {
   <div class="item">
     <div class="details">
       <h3 @click="toggleAccordion">
-        <div>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-            stroke="currentColor" class="w-5 h-5">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-          </svg>
-        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+          class="w-5 h-5">
+          <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+        </svg>
 
         <span>
           <slot name="heading"></slot>
